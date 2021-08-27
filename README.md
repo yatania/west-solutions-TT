831230917e98bf1c4dfe9174bcab3645b0658b2c
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-27 21:29:08 +0300

    added footer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ELSÄSSER KÄLTETECHNIK site
-Stack: HTML5, SCSS, BEM, Bootstrap
+Stack: HTML5, SCSS, BEM, Bootstrap, JS
 Fully responsive layout was made by using Flexbox (mobiles, tablets, laptops, high screens)
 Click the link to see [DEMO LINK](https://yatania.github.io/west-solutions-TT/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const previous = document.querySelector('.buttons__prev');
+const next = document.querySelector('.buttons__next');
+const slides = document.querySelectorAll('.slides__slide');
+const dots = document.querySelectorAll('.dots__dot');
+
+let index = 0;
+
+const activeSlide = n => {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
+  }
+
+  slides[n].classList.add('active');
+};
+
+const activeDot = n => {
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  dots[n].classList.add('active');
+};
+
+const prepareCurrentSlide = currentIndex => {
+  activeSlide(currentIndex);
+  activeDot(currentIndex);
+};
+
+const nextSlide = () => {
+  if (index === slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index === 0) {
+    index = slides.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+};
+
+next.addEventListener('click', nextSlide);
+previous.addEventListener('click', prevSlide);
